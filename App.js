872b95491b2917b0d39982c57d6f2c68965f9aec
Sachin Kumar Faujdar <70964606+mesachinfaujdar@users.noreply.github.com>
2023-12-11 09:57:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import VideoPlayer from './VideoPlayer';
 
+function App() {
+  const [videoId, setVideoId] = useState('');
+  const [videoInfo, setVideoInfo] = useState({});
+
+  useEffect(() => {
+    if (videoId) {
+      fetch(`/api/video/${videoId}`)
+        .then((response) => response.json())
+        .then((data) => setVideoInfo(data));
+    }
+  }, [videoId]);
+
+  const handleChange = (event) => {
+    setVideoId(event.target.value);
+  };
+
+  return (
+    <div className="app">
+      <h1>heLLO! This is my  Video Player</h1>
+      <input
+        type="text"
+        placeholder="Enter unlisted video ID"
+        value={videoId}
+        onChange={handleChange}
+      />
+      {videoInfo.title && <VideoPlayer {...videoInfo} />}
+    </div>
+  );
+}
+
+export default App;
