65bf53408bcb08540f55b78958391605f398541d
Sachin Kumar Faujdar <70964606+mesachinfaujdar@users.noreply.github.com>
2023-12-11 09:47:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const axios = require('axios');
 
+const app = express();
+const port = 3000;
+
+app.get('/api/video/:id', async (req, res) => {
+  const videoId = req.params.id;
+  const API_KEY = '{MY_KEY_1}';
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    const video = response.data.items[0];
+    const videoInfo = {
+      title: video.snippet.title,
+      description: video.snippet.description,
+      thumbnail: video.snippet.thumbnails.default.url,
+      url: `https://www.youtube.com/embed/${videoId}`,
+    };
+    res.json(videoInfo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Error fetching video details.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
